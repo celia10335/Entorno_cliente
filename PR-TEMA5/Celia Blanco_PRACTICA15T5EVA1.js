e4882c89bb359e6934d6339a9601c9e9d116b8f9
Celia <10335@cifpceuta.es>
2022-11-26 01:02:59 +0100

    modified:   PR-TEMA5/Celia Blanco_PRACTICA15T5EVA1.js

--- PR-TEMA5/Celia Blanco_PRACTICA15T5EVA1.js
+++ PR-TEMA5/Celia Blanco_PRACTICA15T5EVA1.js
@@ -1,3 +1,5 @@
+// ORDENADOR
+// Constructor, donde inicializo los atributos y les asigno valores por defecto a aquellos que son opcionales (memoriaRAM, GB_disco y pulgadas)
 function Ordenador(marca, modelo, memoriaRAM = 4, GB_disco = 512, pulgadas = 17) {
     this.marca = marca;
     this.modelo = modelo;
@@ -6,6 +8,7 @@ function Ordenador(marca, modelo, memoriaRAM = 4, GB_disco = 512, pulgadas = 17)
     this.pulgadas = pulgadas;
 
 
+    // Función que devuelve información sobre las propiedades del objeto que la llama
     this.toString = function () {
         return (`marca: ${this.marca}, modelo: ${this.modelo}, memoria RAM: ${this.memoriaRAM}, capacidad de disco: ${this.GB_disco}, pulgadas: ${this.pulgadas}`);
     }
@@ -13,6 +16,7 @@ function Ordenador(marca, modelo, memoriaRAM = 4, GB_disco = 512, pulgadas = 17)
 }
 
 // PORTÁTIL
+// Utilizo la clase "Ordenador" como prototipo para establecer la marca y el modelo, asigno valores por defecto a aquellas propiedades que no son obligatorias al instanciar el objeto. No las tomo del prototipo Ordenador porque tienen otros valores por defecto.
 function Portatil(marca, modelo, memoriaRAM = 4, GB_disco = 256, pulgadas = 13, autonomia = 4) {
     this.__proto__ = new Ordenador(marca, modelo);
     this.memoriaRAM = memoriaRAM;
@@ -21,6 +25,7 @@ function Portatil(marca, modelo, memoriaRAM = 4, GB_disco = 256, pulgadas = 13, 
     this.autonomia = autonomia;
 
 
+    // Función que devuelve información sobre las propiedades del objeto que la llama
     this.toString = function(){
          return (`marca: ${this.marca}, modelo: ${this.modelo}, memoria RAM: ${this.memoriaRAM}, capacidad de disco: ${this.GB_disco}, pulgadas: ${this.pulgadas}, autonomía: ${this.autonomia}`);
     }
