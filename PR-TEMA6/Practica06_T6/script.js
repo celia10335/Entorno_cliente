a897ea6e91b7f96ac29d327d243603765c76a15d
Celia <10335@cifpceuta.es>
2023-01-19 23:25:39 +0100

    new file:   PR-TEMA6/Practica06_T6/index.html 	modified:   PR-TEMA6/Practica06_T6/script.js

--- PR-TEMA6/Practica06_T6/script.js
+++ PR-TEMA6/Practica06_T6/script.js
@@ -1,7 +1,26 @@
 function inspeccionar() {
-    let body = document.getElementsByTagName('body').item(0);
-    let listHijos = body.childNodes;
-    for (let h of listHijos) {
+    let padre = document.getElementsByTagName('body').item(0);
 
+    let info = "";
+
+    let listHijos = padre.childNodes;
+
+
+    function buscaHijos(padre) {
+        let listHijos = padre.childNodes;
+        for (let h of listHijos) {
+            info += "\n" + h.nodeName;
+        }
     }
+
+    for (let h of listHijos) {
+        if (h.hasChildNodes) {
+            buscaHijos(h);
+        }
+        else {
+            info += "\n" + h.nodeName;
+        }
+    }
+
+    console.log(info);
 }
