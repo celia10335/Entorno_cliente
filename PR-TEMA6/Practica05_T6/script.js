25ab155f9354414534bdace81feffe8467310016
Celia <10335@cifpceuta.es>
2023-01-19 01:07:44 +0100

    modified:   PR-TEMA6/Practica05_T6/script.js

--- PR-TEMA6/Practica05_T6/script.js
+++ PR-TEMA6/Practica05_T6/script.js
@@ -2,31 +2,40 @@ function desglosarDom(elemento) {
     let info = '';
     let listaPadres = document.getElementsByTagName(elemento);
 
-    for (let hijo of listaPadres[0].childNodes) {
+    let i = 0;
 
-        info += "<br>&#10043 Nombre de nodo: " + hijo.nodeName + "<br>Tipo: " + hijo.nodeType;
-        if (hijo.nodeValue != null) {
-            info += '<br>Contenido: ' + hijo.nodeValue;
-        }
+    do {
 
-        if (hijo.nodeType == 1 && hijo.hasAttributes) {
-            info += "<br>Atributos: ";
+        for (let hijo of listaPadres[i].childNodes) {
 
-            for (let attr of hijo.attributes) {
-                info += `${attr.name} -> ${attr.value}\n`;
+            info += "<br>&#10043 Nombre de nodo: " + hijo.nodeName + "<br>Tipo: " + hijo.nodeType;
+            if (hijo.nodeValue != null) {
+                info += '<br>Contenido: ' + hijo.nodeValue;
             }
 
-            info += "<hr>";
-        }
+            if (hijo.nodeType == 1 && hijo.hasAttributes) {
+                info += "<br>Atributos: ";
 
-        else {
-            info += "<hr>";
-        }
+                for (let attr of hijo.attributes) {
+                    info += `${attr.name} -> ${attr.value}\n`;
+                }
 
-        if (hijo.nodeType == 1 && hijo.hasChildNodes) {
-            info += desglosarDom(hijo.nodeName);
+                info += "<hr>";
+            }
+
+            else {
+                info += "<hr>";
+            }
+
+            if (hijo.nodeType == 1 && hijo.hasChildNodes) {
+                info += desglosarDom(hijo.nodeName);
+            }
         }
-    }
+        i++;
+
+    } while (i < listaPadres.length);
+
+
 
     return info;
 }
