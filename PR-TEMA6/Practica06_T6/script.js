3dc3446e4ccf712de4eb09289209b3d23c8fa038
Celia <10335@cifpceuta.es>
2023-01-20 21:10:25 +0100

    listo para entregar

--- PR-TEMA6/Practica06_T6/script.js
+++ PR-TEMA6/Practica06_T6/script.js
@@ -1,26 +1,8 @@
-function inspeccionar() {
-    let padre = document.getElementsByTagName('body').item(0);
+// Le paso a la función dos parámetros; el primero será el elemento y el segundo es la expresión regular o texto que hay que localizar y sustituir.
+function bloquearContenido(elemento, regex) {
 
-    let info = "";
+    // Uso el método 'innerHTML' porque de esta manera obtengo en un solo paso todo el contenido del elemento (que será el body cuando llame a la función), incluyendo los nodos textos de todos los nodos etiqueta contenidos en ese elemento.
+    // El elemento de la variable 'regex' se sustituye por una etiqueta span con el texto "CENSURADO"; de esta manera se le puede dar un formato llamativo fácilmente.
+    elemento.innerHTML = elemento.innerHTML.replaceAll(regex, "<span>CENSURADO</span>");
 
-    let listHijos = padre.childNodes;
-
-
-    function buscaHijos(padre) {
-        let listHijos = padre.childNodes;
-        for (let h of listHijos) {
-            info += "\n" + h.nodeName;
-        }
-    }
-
-    for (let h of listHijos) {
-        if (h.hasChildNodes) {
-            buscaHijos(h);
-        }
-        else {
-            info += "\n" + h.nodeName;
-        }
-    }
-
-    console.log(info);
 }
