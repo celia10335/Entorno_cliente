ec7a07876503cc9d56df57b0c74b82089aa70c29
Celia <10335@cifpceuta.es>
2023-01-23 19:40:13 +0100

    terminado

--- PR-TEMA6/Practica08_T6/script.js
+++ PR-TEMA6/Practica08_T6/script.js
@@ -1,19 +1,25 @@
+let i = 1;
+
 function insertaFila() {
+    i++;
     let elemento = document.getElementsByTagName('tbody').item(0);
     let contenido = document.getElementById('texto').value;
     if (contenido == null) {
         contenido = "";
     }
     let filaNueva = document.createElement('tr');
-    filaNueva.innerHTML = `<td>${contenido}</td>
-    <td><button onclick="mayusc()">Mayúsculas</button></td>
-    <td><button>Formato chachi</button></td>`;
+    filaNueva.innerHTML = `<td id="fila${i}">${contenido}</td>
+    <td><button onclick="mayusc('fila${i}')">Mayúsculas</button></td>
+    <td><button onclick="formato('fila${i}')">Formato chachi</button></td>`;
     elemento.appendChild(filaNueva);
     document.getElementById('texto').value = "";
     document.getElementById('texto').focus();
 }
 
-function mayusc() {
-    let celda = document.querySelectorAll('td');
-    console.log(celda);
+function mayusc(id) {
+    document.getElementById(id).classList.toggle('mayusculas');
+}
+
+function formato(id) {
+    document.getElementById(id).classList.toggle('chachi');
 }
