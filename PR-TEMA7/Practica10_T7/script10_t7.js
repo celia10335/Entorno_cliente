72a86a35c843b9b9d55c016ba1cd78c4b30df4fe
Celia <10335@cifpceuta.es>
2023-01-25 16:52:52 +0100

    terminado

--- PR-TEMA7/Practica10_T7/script10_t7.js
+++ PR-TEMA7/Practica10_T7/script10_t7.js
@@ -1,10 +1,14 @@
 function newFondo() {
-    let cuerpo = document.getElementsByTagName("body")[0];
+    // Capturo el elemento "body" por su selector CSS
+    let cuerpo = document.querySelector('body');
+
+    // Añado un evento al documento, tal que al tener una tecla presionada (barra espaciadora) y pulsar la otra (tecla R) genere un número aleatorio. Este número se corresponde con el nombre de una imagen (fondo[numero].jpg).
     document.addEventListener("keydown", function (ev1) {
         document.addEventListener("keypress", function (ev2) {
             if ((ev2.code == "KeyR") && (ev1.code == "Space")) {
                 let random = parseInt(Math.random() * 9);
 
+                // Le asigno esa imagen como fondo del body, y ajusto el tamaño a "cover" para que ocupe el ancho de la pantalla.
                 cuerpo.style.backgroundImage = `url('fondo${random}.jpg')`;
                 cuerpo.style.backgroundSize = "cover";
             }
@@ -13,18 +17,14 @@ function newFondo() {
 
     })
 
+    // Esta es la combinación de teclas que pide el enunciado, pero sólo me lleva a la página una vez. Si quiero cambiar de fondo, debo recargar la página.
+    document.addEventListener("keydown", function (ev) {
+        if ((ev.code == "F12") && (ev.altKey)) {
+            cuerpo.style.backgroundImage = "url('https://source.unsplash.com/random')";
+            cuerpo.style.backgroundSize = "cover";
+        }
+
+
+    })
 
 }
-
-/*
-        document.addEventListener("keypress", function (ev2) {
-            if ((ev2.code == "KeyR") && (ev1.ctrlKey == true)) {
-                cuerpo.style.backgroundColor = "purple";
-            }
-
-        })
-    document.addEventListener("keypress", (ev3) => {
-        console.log(ev3.code);
-    }) */
-
-//this.location = "https://images.unsplash.com/photo-1670922657788-d92272b59c95?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY3NDU5NTE4NQ&ixlib=rb-4.0.3&q=80&w=1080";
