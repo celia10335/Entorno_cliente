31a86ddbd8fb2b053c1e8ac73ecc19ff36932b4e
Celia <10335@cifpceuta.es>
2023-02-08 22:44:39 +0100

    afinar y comentar

--- PR-TEMA7/Practica14_T7/script14_t7.js
+++ PR-TEMA7/Practica14_T7/script14_t7.js
@@ -1,32 +1,18 @@
-function followMe() {
-  //Capturo el cuadrado amarillo por su id
-  let cuadrado = document.getElementById('cuadrado');
-  // El escuchador se lo asigno al documento entero, ya que la función debe funcionar para cualquier posición del cursor dentro de la pantalla del navegador.
-  // El evento a capturar es "mousemove", ya que quiero una continuidad en los movimientos del cursor. Con "mouseover" se movería a trompicones.
-  document.addEventListener('mousemove', function (ev) {
-    // Accedo a las propiedades de posicionamiento del div (top y left) y de esta manera, las puedo ir variando según lo que recibe el escuchador. Le asigno la posición según las coordenadas X e Y del cliente, no me interesa que el cursor pueda moverse si estoy fuera del navegador (desaparecería el cuadradito amarillo).
-    // Ajusto la posición del cursor dentro del elemento restándole a cada posición 45 píxeles.
-    cuadrado.style.top = ev.clientY - 45 + 'px';
-    cuadrado.style.left = ev.clientX - 45 + 'px';
-  });
-}
-
 function circulos() {
   let cuerpo = document.getElementsByTagName('body')[0];
-
-  let circulo = document.createElement('div');
-  circulo.classList.add = 'circle';
-  circulo.style.height = '100px';
-  circulo.style.width = '100px';
-  cuerpo.appendChild(circulo);
-
-  // document.addEventListener('click', function (ev) {
-  //   let circulo = document.createElement('div');
-  //   circulo.classList.add = 'circle';
-  //   circulo.style.height = '100px';
-  //   circulo.style.top = ev.clientY;
-  //   circulo.style.left = ev.clientX;
-  //   cuerpo.appendChild(circulo);
-  // }
-  // )
+  document.addEventListener('mouseup', function (ev) {
+    let circulo = document.createElement('div');
+    circulo.classList.add('circle');
+    let diametro = Math.random() * 200;
+    circulo.style.height = diametro + 'px';
+    circulo.style.width = circulo.style.height;
+    let color_red = Math.random() * 255;
+    let color_green = Math.random() * 255;
+    let color_blue = Math.random() * 255;
+    circulo.style.backgroundColor = `rgb(${color_red}, ${color_green}, ${color_blue})`;
+    circulo.style.top = ev.clientY - diametro / 2 + 'px';
+    circulo.style.left = ev.clientX - diametro / 2 + 'px';
+    cuerpo.appendChild(circulo);
+  }
+  )
 }
