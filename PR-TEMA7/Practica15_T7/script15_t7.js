24ae5067c4b8ff74929e1d7d94d4064f9a72fa9d
Celia <10335@cifpceuta.es>
2023-02-13 23:15:34 +0100

    13febrero

--- PR-TEMA7/Practica15_T7/script15_t7.js
+++ PR-TEMA7/Practica15_T7/script15_t7.js
@@ -1,3 +1,27 @@
-function prometiendo() {
+function prometiendo(t) {
 
+  let tiempo = t;
+
+  let cuentaAtras = setInterval(function () {
+    tiempo--;
+    p.innerHTML = tiempo;
+  }, 1000);
+
+
+  function interrumpir() {
+    clearInterval(cuentaAtras);
+    let p = document.getElementById('p');
+    p.innerHTML = "Se ha pulsado el botón";
+  }
+  // let promesa = new Promise(function (resuelto, rechazado) {
+  //   if (tiempo == 0) {
+  //     resuelto("Tiempo concluido");
+  //   }
+  //   else {
+  //     rechazado("Tiempo interrumpido");
+  //   }
+  // }
+  // )
 }
+
+
