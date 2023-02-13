3fc4df00f82c220d447a6bdb5e42da98d66d8508
Celia <10335@cifpceuta.es>
2023-02-13 23:19:18 +0100

    13febrero

--- PR-TEMA7/Practica15_T7/script15_t7.js
+++ PR-TEMA7/Practica15_T7/script15_t7.js
@@ -1,6 +1,7 @@
 function prometiendo(t) {
 
   let tiempo = t;
+  let p = document.getElementById('p');
 
   let cuentaAtras = setInterval(function () {
     tiempo--;
@@ -10,18 +11,23 @@ function prometiendo(t) {
 
   function interrumpir() {
     clearInterval(cuentaAtras);
-    let p = document.getElementById('p');
     p.innerHTML = "Se ha pulsado el botón";
   }
-  // let promesa = new Promise(function (resuelto, rechazado) {
-  //   if (tiempo == 0) {
-  //     resuelto("Tiempo concluido");
-  //   }
-  //   else {
-  //     rechazado("Tiempo interrumpido");
-  //   }
-  // }
-  // )
+  let promesa = new Promise(function (resuelto, rechazado) {
+    if (tiempo == 0) {
+      resuelto("Tiempo concluido");
+    }
+    else {
+      rechazado("Tiempo interrumpido");
+    }
+  }
+  );
+
+  promesa.then((respuesta) => {
+    p.innerHTML = respuesta;
+  }).catch((mensajeError) => {
+    p.innerHTML = mensajeError;
+  });
 }
 
 
