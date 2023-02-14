410bf72bcafca960e8e5b721e6648d5559895204
celia10335 <113554501+celia10335@users.noreply.github.com>
2023-02-14 13:58:13 +0100

    Add files via upload

--- PR-TEMA7/Practica15_T7/script15_t7.js
+++ PR-TEMA7/Practica15_T7/script15_t7.js
@@ -1,33 +1,38 @@
 function prometiendo(t) {
-
-  let tiempo = t;
   let p = document.getElementById('p');
+  let boton = document.querySelector('button');
 
-  let cuentaAtras = setInterval(function () {
-    tiempo--;
-    p.innerHTML = tiempo;
-  }, 1000);
-
-
-  function interrumpir() {
-    clearInterval(cuentaAtras);
-    p.innerHTML = "Se ha pulsado el botón";
-  }
   let promesa = new Promise(function (resuelto, rechazado) {
-    if (tiempo == 0) {
-      resuelto("Tiempo concluido");
-    }
-    else {
-      rechazado("Tiempo interrumpido");
-    }
-  }
-  );
+    let cuentaAtras = setInterval(function () {
+      t--;
+      p.innerHTML = t;
+      if (t == 0) {
+        clearInterval(cuentaAtras);
+        resuelto('Tiempo concluido');
+      }
+    }, 1000);
 
-  promesa.then((respuesta) => {
-    p.innerHTML = respuesta;
-  }).catch((mensajeError) => {
-    p.innerHTML = mensajeError;
+    boton.addEventListener('click', () => {
+      clearInterval(cuentaAtras);
+      rechazado('Se ha interrumpido la cuenta atrás');
+    });
   });
+
+  promesa
+    .then((respuesta) => {
+      p.innerHTML = respuesta;
+    })
+    .catch((mensajeError) => {
+      p.innerHTML = mensajeError;
+    });
 }
 
+/* let cuentaAtras = setInterval(function () {
+  tiempo--;
+  p.innerHTML = tiempo;
+}, 1000);
 
+function interrumpir() {
+  clearInterval(cuentaAtras);
+  p.innerHTML = 'Se ha pulsado el botón';
+}*/
