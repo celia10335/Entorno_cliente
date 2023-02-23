93e841a66b11ee8de8a81fea691efd44719dc502
Celia <10335@cifpceuta.es>
2023-02-23 20:41:21 +0100

    TERMINADO

--- PR-TEMA8/Practica17_T8/script17_t8.js
+++ PR-TEMA8/Practica17_T8/script17_t8.js
@@ -1,4 +1,5 @@
 export function circulos() {
+  // Con esta línea de código, evito que se despliegue el menú contextual al hacer clic con el botón derecho del ratón.
   document.oncontextmenu = new Function("return false");
   // Capturo el body, ya que de aquí iré "colgando" los "hijos" (que serán los divs-círculos)
   let cuerpo = document.getElementsByTagName('body')[0];
@@ -6,13 +7,11 @@ export function circulos() {
   // El evento que va a capturar es "mousedown".
   document.addEventListener('mousedown', function (ev) {
 
-    console.log(ev.type);
+    // Creo un div al que llamo "círculo". 
+    let circulo = document.createElement('div');
 
+    // Con try ... catch voy a capturar el error en caso de que se pulse un botón del ratón diferente al izquierdo o derecho.
     try {
-
-
-      // Creo un div al que llamo "círculo". 
-      let circulo = document.createElement('div');
 
       //Si se pulsa con el botón principal del ratón, le añado la clase "circle" con la que controlo que el borde sea redondeado en un 50% (o sea, que forme un círculo). En caso contrario, la clase 'square' que lo dibuja como un cuadrado.
       if (ev.button == 0) {
@@ -20,12 +19,13 @@ export function circulos() {
 
       } else if (ev.button == 2) {
         circulo.classList.add('square');
+
       } else {
         throw new Error('Movimiento no permitido');
-
       }
 
-      // Por comodidad, almaceno en una variable el diámetro del círculo, que en realidad será el alto y ancho del div. Le he asignado un número aleatorio entre 50 y 200 píxeles.
+      // A continuación, todas las propiedades del div en cuanto a tamaño y color aleatorios:
+      // Almaceno en una variable el diámetro del círculo, que en realidad será el alto y ancho del div. Le he asignado un número aleatorio entre 50 y 200 píxeles.
       let diametro = Math.floor(Math.random() * 200) + 50;
 
       // Indico las propiedades del círculo: dimensiones y color aleatorio rgb.
@@ -42,12 +42,25 @@ export function circulos() {
       // posiciono el div de manera que el centro coincida con el puntero.
       circulo.style.top = ev.clientY - diametro / 2 + 'px';
       circulo.style.left = ev.clientX - diametro / 2 + 'px';
+
+      // ********* BLOQUE CATCH
     } catch (error) {
-      console.log(error.message);
+      console.error(error.message);
+      alert(error.message);
     }
   }
-
   )
+
+
+  // ******* LANZAR ERROR PARA EL CASO EN QUE SE HAGA DOBLE CLIC CON EL RATÓN:
+  document.addEventListener('dblclick', () => {
+    try {
+      throw new Error('Movimiento no permitido');
+    } catch (error) {
+      console.error(error.message);
+      alert(error.message);
+    }
+  })
 
 
 }
