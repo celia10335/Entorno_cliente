eb470e3839e8ee092d36d4ff36392e30f0315112
Celia <10335@cifpceuta.es>
2023-02-24 07:23:48 +0100

    24febrero

--- PR-TEMA8/Practica16_T8-v2/script.js
+++ PR-TEMA8/Practica16_T8-v2/script.js
@@ -4,9 +4,8 @@ function contarVisitas() {
   // Capturo el elemento #info que mostrará el mensaje correspondiente cuando el usuario pulse los botones.
   let info = document.getElementById('info');
 
-  cuadro.innerHTML = '<div><h1>Esta página dice</h1><p>Esta página usa una cookie para almacenar el número de visitas que hace cada usuario. Si está conforme con ello, pulse "Sí".</p><p>De otro modo, la página no realizará su labor y eliminará las cookies existentes.</p><button id="aceptar">Aceptar</button><button id="cancelar">Cancelar</button></div>';
 
-  //Capturo los elementos con los que va a interactuar el usuario: botones de aceptar, cerrar sesión y cancelar. También capturo el cuadro de información, que se mostrará sólo al principio.
+  //Capturo los elementos con los que va a interactuar el usuario: botones de aceptar, cerrar sesión y cancelar.
   let botonAceptar = document.getElementById('aceptar');
   let botonCancelar = document.getElementById('cancelar');
   let botonCerrar = document.getElementById('cerrar');
@@ -15,7 +14,7 @@ function contarVisitas() {
   // Al pulsar el botón "Aceptar", se crea una cookie que almacena el valor de "visitas". Al mismo tiempo, este valor se incrementa en 1 cada vez que se pulse el botón.
   botonAceptar.addEventListener('click', () => {
     document.cookie = 'visitas=' + 2;
-    cuadro.innerHTML = '';
+    cuadro.classList.add('no-visible');
 
     info.textContent = 'Ya nos has visitado 1 vez';
 
@@ -27,10 +26,12 @@ function contarVisitas() {
   // Con el método split, extraigo el valor almacenado en la cookie, ya que lo necesito para mostrar el mensaje de información.
   let [nombre, valor] = document.cookie.split('=');
 
+  console.log(valor);
+
   if (valor >= 1) {
     info.textContent = `Ya nos has visitado ${valor} veces`;
     botonCerrar.innerHTML = '<button>Cerrar sesión</button>';
-    cuadro.innerHTML = '';
+    //cuadro.setAttribute('class', 'no-visible');
   }
 
   valor++;
@@ -41,8 +42,9 @@ function contarVisitas() {
     document.cookie = 'visitas = ' + 0;
     info.textContent = '';
     botonCerrar.innerHTML = '';
+    cuadro.removeAttribute('class');
 
-    cuadro.innerHTML = '<div><h1>Esta página dice</h1><p>Esta página usa una cookie para almacenar el número de visitas que hace cada usuario. Si está conforme con ello, pulse "Sí".</p><p>De otro modo, la página no realizará su labor y eliminará las cookies existentes.</p><button id="aceptar">Aceptar</button><button id="cancelar">Cancelar</button></div>';
+    // cuadro.innerHTML = '<div><h1>Esta página dice</h1><p>Esta página usa una cookie para almacenar el número de visitas que hace cada usuario. Si está conforme con ello, pulse "Sí".</p><p>De otro modo, la página no realizará su labor y eliminará las cookies existentes.</p><button id="aceptar">Aceptar</button><button id="cancelar">Cancelar</button></div>';
   });
 
 
